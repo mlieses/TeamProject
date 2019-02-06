edcda78881b9ba82bcad341488a0f1ecefa49fa9
eun89 <eun89@121.174.122.204>
2019-02-06 22:06:14 +0900

    search DAO sql 수정

--- TeamProject/src/home/SearchDAO.java
+++ TeamProject/src/home/SearchDAO.java
@@ -129,7 +129,7 @@ and h.people = '소';
 								+"and b.book_check = 0 "
 								+"group by b.room_no) c "
 								+"on h.room_no = c.room_no "
-								+"where c.ch != 12 "
+								+"where (c.ch != 12 or c.ch is null)"
 								+"and h.room_type = ? "
 								+"and a.a_address like '%"+location+"?%' "
 								+"and h.people = ? ";
