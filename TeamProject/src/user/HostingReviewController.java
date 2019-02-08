3218b8b9c06c84a4f60cf1463f2c8f8914ecd800
eun89jin <eun89jin@naver.com>
2019-02-08 11:33:53 +0900

    호스트 리뷰 DB

---
+++ TeamProject/src/user/HostingReviewController.java
@@ -0,0 +1,61 @@
+package user;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/HostingReviewController.do")
+public class HostingReviewController extends HttpServlet {
+
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		doProc(req, resp);
+	}
+
+	
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		doProc(req, resp);
+	}
+	
+	protected void doProc(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		req.setCharacterEncoding("UTF-8");
+		String host_id = req.getParameter("host_id");
+		String room_no = req.getParameter("room_no");
+		HostingReviewDAO dao = new HostingReviewDAO();
+		ArrayList<HostingReviewInfoDTO> SpaceList = null;
+		ArrayList<HostingReviewDTO> ReviewList = null;
+		
+		if(host_id==null){
+			resp.setContentType("text/html; charset=UTF-8");
+			PrintWriter out = resp.getWriter();
+			out.println("<script>");
+			out.println("alert('호스트 로그인 후 이용 바랍니다')");
+			out.println("history.go(-1)");
+			out.println("</script>");
+			return;
+			
+		}else if(room_no == null){// 처음 실행하거나 전체선택 버튼 눌러서 공간 전체의 값을 얻어와야 할때
+			SpaceList = dao.SpaceList(host_id);
+			ReviewList = dao.ReviewList(host_id);
+			
+		}else if(room_no != null){
+			SpaceList = dao.SpaceList(host_id);
+			ReviewList = dao.ReviewList(host_id, room_no);
+			
+		}
+		req.setAttribute("sList", SpaceList);
+		req.setAttribute("rList", ReviewList);
+		
+		RequestDispatcher dis = req.getRequestDispatcher("/user/hostReviewList.jsp");
+		dis.forward(req, resp);
+		
+		
+	}
+
+}
--- TeamProject/src/user/HostingReviewInfoDTO.java
+++ TeamProject/src/user/HostingReviewInfoDTO.java
@@ -12,13 +12,13 @@ public class HostingReviewInfoDTO {
 	private String pic1;
 	
 	
-	/**1-1. 호스트의 공간 평점과 평점 갯수 에 관한 정보*/
-	private int pointAvg;
-	private int po1cnt;
-	private int po2cnt;
-	private int po3cnt;
-	private int po4cnt;
-	private int po5cnt;
+//	/**1-1. 호스트의 공간 평점과 평점 갯수 에 관한 정보*/
+//	private int pointAvg;
+//	private int po1cnt;
+//	private int po2cnt;
+//	private int po3cnt;
+//	private int po4cnt;
+//	private int po5cnt;
 	
 	
 	/**getter setter*/
@@ -52,42 +52,42 @@ public class HostingReviewInfoDTO {
 	public void setPic1(String pic1) {
 		this.pic1 = pic1;
 	}
-	public int getPointAvg() {
-		return pointAvg;
-	}
-	public void setPointAvg(int pointAvg) {
-		this.pointAvg = pointAvg;
-	}
-	public int getPo1cnt() {
-		return po1cnt;
-	}
-	public void setPo1cnt(int po1cnt) {
-		this.po1cnt = po1cnt;
-	}
-	public int getPo2cnt() {
-		return po2cnt;
-	}
-	public void setPo2cnt(int po2cnt) {
-		this.po2cnt = po2cnt;
-	}
-	public int getPo3cnt() {
-		return po3cnt;
-	}
-	public void setPo3cnt(int po3cnt) {
-		this.po3cnt = po3cnt;
-	}
-	public int getPo4cnt() {
-		return po4cnt;
-	}
-	public void setPo4cnt(int po4cnt) {
-		this.po4cnt = po4cnt;
-	}
-	public int getPo5cnt() {
-		return po5cnt;
-	}
-	public void setPo5cnt(int po5cnt) {
-		this.po5cnt = po5cnt;
-	}
+//	public int getPointAvg() {
+//		return pointAvg;
+//	}
+//	public void setPointAvg(int pointAvg) {
+//		this.pointAvg = pointAvg;
+//	}
+//	public int getPo1cnt() {
+//		return po1cnt;
+//	}
+//	public void setPo1cnt(int po1cnt) {
+//		this.po1cnt = po1cnt;
+//	}
+//	public int getPo2cnt() {
+//		return po2cnt;
+//	}
+//	public void setPo2cnt(int po2cnt) {
+//		this.po2cnt = po2cnt;
+//	}
+//	public int getPo3cnt() {
+//		return po3cnt;
+//	}
+//	public void setPo3cnt(int po3cnt) {
+//		this.po3cnt = po3cnt;
+//	}
+//	public int getPo4cnt() {
+//		return po4cnt;
+//	}
+//	public void setPo4cnt(int po4cnt) {
+//		this.po4cnt = po4cnt;
+//	}
+//	public int getPo5cnt() {
+//		return po5cnt;
+//	}
+//	public void setPo5cnt(int po5cnt) {
+//		this.po5cnt = po5cnt;
+//	}
 	
 	
 	
