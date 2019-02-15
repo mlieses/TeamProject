3c9dbe357b69837320a2358e16ebe53794a5ac40
eun89 <eun89@DESKTOP-SVS0OPR>
2019-02-16 03:59:07 +0900

    호스트 리뷰 페이지

--- TeamProject/src/user/HostingReviewController.java
+++ TeamProject/src/user/HostingReviewController.java
@@ -43,10 +43,12 @@ public class HostingReviewController extends HttpServlet {
 		}else if(room_no == null){// 처음 실행하거나 전체선택 버튼 눌러서 공간 전체의 값을 얻어와야 할때
 			SpaceList = dao.SpaceList(host_id);
 			ReviewList = dao.ReviewList(host_id);
+			req.setAttribute("check", 0);
 			
 		}else if(room_no != null){
 			SpaceList = dao.SpaceList(host_id);
 			ReviewList = dao.ReviewList(host_id, room_no);
+			req.setAttribute("check", 1);
 			
 		}
 		req.setAttribute("sList", SpaceList);
